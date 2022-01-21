2b6677704f4170ca775d848784efc565012d61ae
Akeju Oluwanifemi <akejunifemi11@gmail.com>
2022-01-21 18:31:28 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function show(){
+    document.getElementById("fa-times").style.display ="flex";
+    document.getElementById("fa-bars").style.display ="none";
+    document.getElementById("mobile-menu").style.width ="300px";
+
+
+
+}
+
+function hide(){
+    document.getElementById("fa-times").style.display ="none";
+    document.getElementById("fa-bars").style.display ="flex";
+    document.getElementById("mobile-menu").style.width ="0px";
+    
+
+
+
+}
