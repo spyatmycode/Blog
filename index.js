434f5363d2b2b0f9e0a38170833e6fd1b3ccdd7b
Akeju Oluwanifemi <akejunifemi11@gmail.com>
2022-01-28 17:47:52 +0100

    Omo, I did Alot Here...Can't remeber all
    But more Vanilla JS
    
    And more heart into it...
    
    I love my Self

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function show(){
     document.getElementById("fa-times").style.display ="flex";
     document.getElementById("fa-bars").style.display ="none";
-    document.getElementById("mobile-menu").style.width ="300px";
+    document.getElementById("mobile-menu").style.width ="100vw";
 
 
 
@@ -15,4 +15,36 @@ function hide(){
 
 
 
+}
+
+
+function darkmode(element){
+
+    var currentBackgroundColor = element.style.backgroundColor;
+
+    if (currentBackgroundColor =="floralwhite") {
+        element.style.backgroundColor = "black";
+        document.getElementById("maintext").style.color = "white";
+        document.getElementById("header").style.backgroundColor = "black";
+        document.getElementById("darklight").innerHTML ="Light Mode";
+        
+    } else {
+        element.style.backgroundColor = "floralwhite";
+        
+        document.getElementById("maintext").style.color = "dodgerblue";
+        document.getElementById("header").style.backgroundColor = "floralwhite";
+        document.getElementById("darklight").innerHTML ="Dark Mode";
+// Copy Right Ohhh, Hire Me, Akeju Oluwanifemi
+
+        
+    }
+
+    
+
+    
+
+  
+    
+    
+    
 }
